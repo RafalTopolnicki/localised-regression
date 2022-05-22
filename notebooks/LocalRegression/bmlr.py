3f14db9f58f1f5d5f1e8c7a9c0de67749f2349c6
RafalTopolnicki <rafal.topolnicki@gmail.com>
2022-05-22 21:53:36 +0200

    PCA, black

--- notebooks/LocalRegression/bmlr.py
+++ notebooks/LocalRegression/bmlr.py
@@ -7,8 +7,7 @@ from .polynomial_regression import PolynomialRegression
 
 
 class BMLR:
-    def __init__(self, cut, M, epsilon=1, substitution_policy='global',
-                 degree=1):
+    def __init__(self, cut, M, epsilon=1, substitution_policy="global", degree=1, max_pca_components=None):
         self.cut = cut
         self.M = M
         self.substitution_policy = substitution_policy
@@ -20,12 +19,13 @@ class BMLR:
         self.epsilon = epsilon
         self.return_nans = False
         self.degree = degree
+        self.max_pca_components = max_pca_components
         self.global_model = PolynomialRegression(degree=self.degree)
 
     def fit(self, x, y):
-        self.__fit__(x=x, y=y, epsilon=self.epsilon, M=self.M)
+        self.__fit__(x=x, y=y, epsilon=self.epsilon, M=self.M, max_pca_components=self.max_pca_components)
 
-    def __fit__(self, x, y, epsilon, M):
+    def __fit__(self, x, y, epsilon, M, max_pca_components):
         self.epsilon = epsilon
         self.M = M
         self.npts = x.shape[0]
@@ -34,31 +34,31 @@ class BMLR:
         self.fitted = True
 
         # fit global model
-        if self.substitution_policy == 'global':
+        if self.substitution_policy == "global":
             self.global_model.fit(x, y)
 
         for r_ in range(M):
             bm = BallMapper(points=x, coloring_df=pd.DataFrame(y), epsilon=epsilon, shuffle=True)
             self.ball_mappers.append(bm)
             for node_id in bm.Graph.nodes:
-                ball_pts_ind = bm.Graph.nodes[node_id]['points covered']
+                ball_pts_ind = bm.Graph.nodes[node_id]["points covered"]
                 if len(ball_pts_ind) >= self.cut:
                     x_ball = x[ball_pts_ind, :]
                     y_ball = y[ball_pts_ind]
-                    model = PolynomialRegression(degree=self.degree)
+                    model = PolynomialRegression(degree=self.degree, max_pca_components=max_pca_components)
                     model.fit(x_ball, y_ball)
-                    bm.Graph.nodes[node_id]['model'] = model
+                    bm.Graph.nodes[node_id]["model"] = model
                 else:
-                    if self.substitution_policy == 'global':
-                        bm.Graph.nodes[node_id]['model'] = self.global_model
+                    if self.substitution_policy == "global":
+                        bm.Graph.nodes[node_id]["model"] = self.global_model
                     else:
                         # find nearest big ball
                         min_dist = np.Inf
                         min_id = None
                         small_ball_mean = np.mean(x[ball_pts_ind, :], axis=0)
                         for big_node_id in bm.Graph.nodes:
-                            big_pts_ids = bm.Graph.nodes[big_node_id]['points covered']
-                            #if len(big_pts_ids) >= self.cut:
+                            big_pts_ids = bm.Graph.nodes[big_node_id]["points covered"]
+                            # if len(big_pts_ids) >= self.cut:
                             big_ball_mean = np.mean(x[big_pts_ids, :], axis=0)
                             dist = np.linalg.norm(small_ball_mean - big_ball_mean)
                             if dist < min_dist:
@@ -74,17 +74,17 @@ class BMLR:
                         #                      f'points. Decrease cut or increase epsilon!')
                         #     #self.return_nans = True
                         #     #continue
-                        big_pts_ids = bm.Graph.nodes[min_id]['points covered']
+                        big_pts_ids = bm.Graph.nodes[min_id]["points covered"]
                         # join the points from big ball and query ball and fit the linear model
                         all_pts_ids = big_pts_ids + ball_pts_ind
-                        #lm_big = LinearRegression().fit(x[all_pts_ids, :], y[all_pts_ids])
-                        model_big = PolynomialRegression(degree=self.degree)
+                        # lm_big = LinearRegression().fit(x[all_pts_ids, :], y[all_pts_ids])
+                        model_big = PolynomialRegression(degree=self.degree, max_pca_components=max_pca_components)
                         model_big.fit(x[all_pts_ids, :], y[all_pts_ids])
-                        bm.Graph.nodes[node_id]['model'] = model_big
+                        bm.Graph.nodes[node_id]["model"] = model_big
 
     def predict(self, x_test):
         if not self.fitted:
-            raise ValueError('Cannot run predict(). Run fit() first')
+            raise ValueError("Cannot run predict(). Run fit() first")
 
         npts_test = x_test.shape[0]
         yhat = np.zeros(npts_test)
@@ -103,30 +103,29 @@ class BMLR:
                     # each of these balls covers several points from the trainig set
                     # here we get a list of training points ids
                     for node_idx in ball_idx:
-                        yhat[pt_id] += bm.Graph.nodes[node_idx]['model'].predict(xp)
-                        #yhat[pt_id] += np.matmul(bm.Graph.nodes[node_idx]['beta'], xp) + bm.Graph.nodes[node_idx]['intercept']
+                        yhat[pt_id] += bm.Graph.nodes[node_idx]["model"].predict(xp)
                         counts[pt_id] += 1.0
                 else:
                     pass
         yhat = np.array(yhat)
-        #counts = np.array(counts).reshape(-1, 1) # FIXME: why reshape?
+        # counts = np.array(counts).reshape(-1, 1) # FIXME: why reshape?
         counts = np.array(counts)
         yhat /= counts
         return yhat
-    
+
     def get_params(self, deep=True):
         out = dict()
-        out['epsilon'] = self.epsilon
-        out['M'] = self.M
-        out['substitution_policy'] = self.substitution_policy
-        out['cut'] = self.cut
+        out["epsilon"] = self.epsilon
+        out["M"] = self.M
+        out["substitution_policy"] = self.substitution_policy
+        out["cut"] = self.cut
         return out
-    
+
     def set_params(self, **params):
         for key, value in params.items():
             setattr(self, key, value)
         return self
-        
+
     def score(self, x, y):
         yhat = self.predict(x)
         rmse = mean_squared_error(yhat, y, squared=False)
--- notebooks/LocalRegression/ml_models.py
+++ notebooks/LocalRegression/ml_models.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.svm import SVR # for building SVR model
+from sklearn.svm import SVR  # for building SVR model
 import catboost as cb
 from LocalRegression.bmlr import BMLR
 from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
@@ -10,8 +10,7 @@ from pyearth import Earth
 
 
 def model_random_forest_params(x, y):
-    param_grid = {'n_estimators': [10, 100, 500, 1000],
-                  'min_samples_split': [2, 3, 5]}
+    param_grid = {"n_estimators": [10, 100, 500, 1000], "min_samples_split": [2, 3, 5]}
     rf = RandomForestRegressor()
     sh = HalvingGridSearchCV(rf, param_grid, cv=3, factor=2).fit(x, y)
     return sh.best_params_
@@ -28,14 +27,14 @@ def model_random_forest(x, y, x_test, y_test, params):
 
 def model_bmlr_params(x, y, cut, M, degree):
     epsilon_trial = (np.mean(np.std(x, axis=0))) / np.sqrt(x.shape[0]) * 3
-    param_grid = {'epsilon': [epsilon_trial * x for x in [0.5, 0.8, 1.0, 1.5, 2.0, 2.5, 3.0]]}
-    bmlr = BMLR(cut=cut, M=M, substitution_policy='nearest', degree=degree)
+    param_grid = {"epsilon": [epsilon_trial * x for x in [0.5, 0.8, 1.0, 1.5, 2.0, 2.5, 3.0]]}
+    bmlr = BMLR(cut=cut, M=M, substitution_policy="nearest", degree=degree)
     sh = HalvingGridSearchCV(bmlr, param_grid, cv=3, factor=3).fit(x, y)
     return sh.best_params_
 
 
 def model_bmlr(x, y, x_test, y_test, cut, M, degree, params):
-    model = BMLR(cut=cut, M=M, substitution_policy='nearest', degree=degree, **params)
+    model = BMLR(cut=cut, M=M, substitution_policy="nearest", degree=degree, **params)
     model.fit(x, y)
     pred = model.predict(x_test)
     score = mean_squared_error(y_test, pred, squared=False)
@@ -46,7 +45,7 @@ def model_bmlr(x, y, x_test, y_test, cut, M, degree, params):
 def model_catboost(x, y, x_test, y_test):
     train_dataset = cb.Pool(x, y)
     test_dataset = cb.Pool(x_test, y_test)
-    model = cb.CatBoostRegressor(loss_function='RMSE', verbose=0)
+    model = cb.CatBoostRegressor(loss_function="RMSE", verbose=0)
     model.fit(train_dataset)
     pred = model.predict(test_dataset)
     score = mean_squared_error(y_test, pred, squared=False)
@@ -55,17 +54,14 @@ def model_catboost(x, y, x_test, y_test):
 
 
 def model_svr_params(x, y):
-    param_grid = {'C': [0.1, 1, 10, 100, 500, 1000],
-                  'degree': [2],
-                  'epsilon': [0.01, 0.1, 1, 10]
-                  }
-    svr = SVR(kernel='rbf')
+    param_grid = {"C": [0.1, 1, 10, 100, 500, 1000], "degree": [2], "epsilon": [0.01, 0.1, 1, 10]}
+    svr = SVR(kernel="rbf")
     sh = HalvingGridSearchCV(svr, param_grid, cv=3, factor=3).fit(x, y)
     return sh.best_params_
 
 
 def model_svr(x, y, x_test, y_test, params):
-    model = SVR(kernel='rbf', **params)
+    model = SVR(kernel="rbf", **params)
     model.fit(x, y)
     pred = model.predict(x_test)
     score = mean_squared_error(y_test, pred, squared=False)
@@ -74,8 +70,7 @@ def model_svr(x, y, x_test, y_test, params):
 
 
 def model_mars_params(x, y):
-    param_grid = {'max_terms': [5, 10, 100, 200],
-                 'max_degree': [1, 2, 3, 4]}
+    param_grid = {"max_terms": [5, 10, 100, 200], "max_degree": [1, 2, 3, 4]}
     mars = Earth()
     sh = HalvingGridSearchCV(mars, param_grid, cv=3, factor=3).fit(x, y)
     return sh.best_params_
@@ -83,7 +78,7 @@ def model_mars_params(x, y):
 
 def model_mars(x, y, x_test, y_test, params):
     model = Earth(**params)
-    model.fit(x,y)
+    model.fit(x, y)
     pred = model.predict(x_test)
     score = mean_squared_error(y_test, pred, squared=False)
     mape = mean_absolute_percentage_error(y_test, pred)
